97f6443b721ebeec0680aea2cefb3d5aa36945ec
piyush-github12 <piyushgithubaccouunt@gmail.com>
2026-04-19 12:07:06 +0530

    feat: implement aggregator application model and schema with supporting sample data files

--- src/model/aggregatorApplication.model.js
+++ src/model/aggregatorApplication.model.js
@@ -24,6 +24,26 @@ const aggregatorApplicationSchema = new Schema(
             required: [true, 'Phone number is required'],
             trim: true,
         },
+        address: {
+            type: String,
+            required: [true, 'Address is required'],
+            trim: true,
+        },
+        pincode: {
+            type: String,
+            required: [true, 'Pincode is required'],
+            trim: true,
+        },
+        state: {
+            type: String,
+            required: [true, 'State is required'],
+            trim: true,
+        },
+        country: {
+            type: String,
+            required: [true, 'Country is required'],
+            trim: true,
+        },
         companyName: {
             type: String,
             required: [true, 'Company name is required'],
